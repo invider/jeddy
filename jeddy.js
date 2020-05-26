7a0c0a1edfbb9abf67a8dfcb7ee02e1eac3a9001
Igor Khotin <khotin@gmx.com>
2020-05-27 02:04:50 +0300

    [+] ctrl+s to save

--- jeddy.js
+++ jeddy.js
@@ -1,10 +1,68 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 let port = 9101
 
 console.log('Jeddy Text Editor')
 
+app.use(bodyParser.text())
+
 app.use('/', express.static('pub'))
+
+const jopen = '/jed/open*'
+app.get(jopen, (req, res, next) => {
+    const origPath = req.path.substring(jopen.length)
+
+    // guard against empty path
+    if (origPath === '') {
+        res.status(404)
+        res.send('No file specified')
+        return
+    }
+
+    const path = './' + origPath
+    console.log('path: ' + path)
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.status(404).send('Not Found: ' + path)
+
+        } else {
+            res.type('.txt')
+            res.status(200)
+            res.send(data)
+        }
+    })
+})
+
+const jsave = '/jed/save*'
+app.post(jsave, (req, res, next) => {
+    const origPath = req.path.substring(jopen.length)
+
+    // guard against empty path
+    if (origPath === '') {
+        res.status(500)
+        res.send('Unable to save - no file specified')
+        return
+    }
+
+    const path = './' + origPath
+    console.log('saving: ' + path)
+    console.log('data: ' + req.body.toString())
+    console.log('----')
+
+    fs.writeFile(path, req.body, (err) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send(`Unable to save ${path}`)
+        } else {
+            res.status(200).send('Saved')
+        }
+    })
+})
+
 
 app.listen(port, () => console.log('Listening at http://localhost:' + port))
--- pub/jed.js
+++ pub/jed.js
@@ -1,1 +1,49 @@
 console.log('hi')
+
+function save() {
+    const path = window.location.hash.substring(1)
+    const jed = document.getElementById('jed')
+    const text = jed.innerHTML
+
+    const url = 'jed/save/' + path
+    console.log(`saving: ${url}`)
+    console.log(text)
+
+    fetch(url, {
+        method: 'post',
+        body: text,
+    }).then(res => res.text())
+    .then(text => {
+        console.log('text')
+    })
+}
+
+function sync() {
+    const path = window.location.hash.substring(1)
+
+    const url = 'jed/open/' + path
+    console.log(`loading: ${url}`)
+
+    fetch(url)
+        .then(res => res.text())
+        .then(text => {
+            const jed = document.getElementById('jed')
+            jed.innerHTML = text
+        })
+}
+
+window.onload = function() {
+    sync()
+}
+
+window.onhashchange = function() {
+    sync()
+}
+
+window.onkeydown = function(e) {
+    if (e.ctrlKey && e.code === 'KeyS') {
+        save()
+        e.preventDefault()
+        e.stopPropagation()
+    }
+}
