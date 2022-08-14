e21c330b8977e7762f91e5493f8b89c81f900d4d
Igor Khotin <khotin@gmx.com>
2022-08-14 12:33:47 +0300

    [*] ml update

--- jeddy.ml
+++ jeddy.ml
@@ -1,3 +1,12 @@
+> sandbox buffers
+> cache changes in localstore on dirty each 3s or each N strokes
+> ability to browse localstore buffers
+> prevent save of read-only files (like help)
+> edit jeddy config as a text file
+> more expressive dirty flag (change frame color?)
+> more expressive status line - show 'editing', 'home', enabled theme mode for 3s etc...
+> terminal themes
+> simple black and white themes
 V a status line to show 'Saved to ...' messages
 V non-editable directory listing
 
